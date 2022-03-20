4497a3fe2a2b5aab75c1c76dd57fe83e336ea4c6
Mariya Senina <senina.m.m@gmail.com>
2022-03-20 17:05:14 +0300

    working task f

--- src/week_2/task_f.cc
+++ src/week_2/task_f.cc
@@ -6,52 +6,27 @@
 using namespace std;
 
 int main(){
-    ifstream fin("/home/senina/Desktop/itmo/algorithms/build/week_2/number.in");
-	ofstream fout("/home/senina/Desktop/itmo/algorithms/build/week_2/number.out");
-	// ifstream fin("number.in");
-	// ofstream fout("number.out");
+    ifstream fin("number.in");
+    ofstream fout("number.out");
 
-
-    //a >= b
     auto cmp = [](string a, string b) {
-        for (int i = 0; i < min(a.size(), b.size()); i++){
-            // cout << "------ a=" << a << " a.size=" << a.size() << " b.size=" << b.size() << " b=" << b << "-----" << endl;
-            if((char)a[i] > (char)b[i]) return 1; // cout << "at i=" << i << " a[i] > b[i]" << endl;
-            if((char)a[i] < (char)b[i]) return 0; // cout << "at i=" << i << " a[i] < b[i]" << endl;
-        } 
-        // if( (a.size() > b.size()) && ( (int) a[b.size()] > 48) ) return 1; //48 is zero at ascii table
-        // else if ( (a.size() < b.size()) && ( (int) b[a.size()] > 48) ) return 0;
-        // else return (int)(a.size() <= b.size());
-
-        if(a.size() == b.size()) return 1;
-        else if(a.size() > b.size()){
-            if((int) a[b.size()] > 48) return 1;
-            else return 0;
-        }else {
-            if((int) b[a.size()] > 48) return 0;
-            else return 1;
-        }
+        return a + b > b + a;
     };
 
-    set<string, decltype(cmp)> numbers(cmp);
+    multiset<string, decltype(cmp)> numbers(cmp);
 
     string line;
-    while (!fin.eof()){
+    while (true){
         fin >> line;
-        // cout << "new line:" << line << endl;
+        if (fin.eof()) {
+			break;
+		}
         numbers.insert(line);
-    
-        // std::set<string>::iterator it;
-        // for(it = (numbers).begin(); it != (numbers).end(); it++){
-        //     cout << *it << " ";
-        // }
-        // cout << endl;
     }
 
-    std::set<string>::iterator it;
+    std::multiset<string>::iterator it;
     for(it = (numbers).begin(); it != (numbers).end(); it++){
         fout << *it;
-        // cout << *it;
     }
 
 	fin.close();
--- src/week_2/task_g.cc
+++ src/week_2/task_g.cc
@@ -8,33 +8,19 @@ int main() {
     string line;
     cin >> line;
 
-    int line_nums[line.size()];
+    int line_nums[];
     int weights[26]; // array
     for(int i = 0; i < line.size(); i++){
-        line_nums[i] = int(line[i]) - 97; //97 -- code of "a" in ascii table
-        cout << "line_nums[" << i << "]=" << line_nums[i] << " ";
+        line_nums[int(line[i]) - 97]++; //97 -- code of "a" in ascii table
+        cout << "line_nums[" << int(line[i]) - 97 << "]=" << line_nums[int(line[i]) - 97] << " ";
     }
     cout << endl;
 
     for(int i = 0; i < 25; i++){
-        cin >> 
-    }
-   
-   
-   
-    vector<string> numbers;
-
-    string line;    
-    getline(cin, line);
-    numbers.insert(numbers.begin(), line);
-
-    while (getline(cin, line)){
-        int index = find_index(line, &numbers, 0, numbers.size());
-        numbers.insert(numbers.begin() + index, line);
+        cin >> weights[i];
     }
 
-    for(int i = 0; i < numbers.size(); i++){
-        cout << numbers[i];
-    }
-    cout << endl;
+    sort_weights()
+
+    
 }
