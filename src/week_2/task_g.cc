e67f66c69c780e87bb2bf9ed4a671b8a760937b6
Mariya Senina <senina.m.m@gmail.com>
2022-03-26 22:19:57 +0300

    logic exept sort done in task g

--- src/week_2/task_g.cc
+++ src/week_2/task_g.cc
@@ -4,12 +4,23 @@
 
 using namespace std;
 
+void sort_weights(int n, int* weights, int* line_numbers, char* alphabet){
+    int max_weight, corresponging_i;
+	for (int i = 1; i < n; i++) {
+		max_weight = weights[i];
+        //sort from bigger to smaller by weights all arrays
+	}
+}
+
 int main() {
     string line;
-    cin >> line;
+    cin >> line; //string line
+    int n = line.length();
 
-    int line_nums[];
-    int weights[26]; // array
+    char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j', 'k', 'm', 'n',
+     'l', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z'};
+    int line_nums[26]; //frequency of numbers
+    int weights[26]; //weight of numbers
     for(int i = 0; i < line.size(); i++){
         line_nums[int(line[i]) - 97]++; //97 -- code of "a" in ascii table
         cout << "line_nums[" << int(line[i]) - 97 << "]=" << line_nums[int(line[i]) - 97] << " ";
@@ -20,7 +31,26 @@ int main() {
         cin >> weights[i];
     }
 
-    sort_weights()
+    sort_weights(n, weights, line_nums, alphabet);
 
-    
+    char output[n];
+    int k = 0;
+    int it = 0;
+    while (k < 26){
+        if(line_nums[k] >= 2){
+            output[k] = alphabet[k];
+            output[n - k - 1] = alphabet[k];
+            line_nums[k] -=2;
+        }else{
+            k++;
+        }
+    }
+
+    it = k;
+    k = 0;
+    while (k < 26 && line_nums[k] == 1){
+        output[it] = alphabet[k];
+        it++;
+        k++;
+    }
 }
